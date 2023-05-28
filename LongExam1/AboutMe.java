28e7d80fd5870217506a396be08fe42af8913a4d
Andrei Jefferson S. Aseron <111546580+Enabuki@users.noreply.github.com>
2023-05-29 07:58:47 +0800

    Update AboutMe.java

--- LongExam1/AboutMe.java
+++ LongExam1/AboutMe.java
@@ -1,43 +1,140 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
-class AboutMe extends JFrame {
+public class AboutMe extends JFrame {
     private JPanel contentPane;
+    private JLabel lblPortfolio;
+    private JLabel lblHome;
+    private JLabel lblAboutMe;
+    private JLabel lblAchievements;
+    private JLabel lblContact;
 
     public AboutMe() {
-        setTitle("ABOUT ME");
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setBounds(100, 100, 450, 487);
+        setTitle("PORTFOLIO");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+
         contentPane = new JPanel();
-        contentPane.setBackground(new Color(222, 184, 135));
+        contentPane.setBackground(Color.BLACK);
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
         setContentPane(contentPane);
         contentPane.setLayout(null);
 
-        JLabel lblAboutMe = new JLabel("About Me");
-        lblAboutMe.setFont(new Font("Maiandra GD", Font.PLAIN, 15));
-        lblAboutMe.setBounds(187, 11, 74, 19);
-        contentPane.add(lblAboutMe);
+        lblPortfolio = new JLabel("PORTFOLIO");
+        lblPortfolio.setBounds(78, 47, 178, 30);
+        lblPortfolio.setForeground(Color.WHITE);
+        lblPortfolio.setHorizontalAlignment(SwingConstants.CENTER);
+        lblPortfolio.setFont(new Font("Maiandra GD", Font.PLAIN, 24));
+        contentPane.add(lblPortfolio);
 
-        JButton btnClose = new JButton("Close");
-        btnClose.setFont(new Font("Maiandra GD", Font.PLAIN, 11));
-        btnClose.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                AboutMe.this.dispose();
+        // Retrieve screen dimensions
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = screenSize.width;
+        int screenHeight = screenSize.height;
+
+        lblHome = new JLabel("HOME");
+        lblHome.setForeground(new Color(255, 255, 255));
+        lblHome.setFont(new Font("Maiandra GD", Font.PLAIN, 15));
+        lblHome.setBounds(915, 45, 107, 50);
+        contentPane.add(lblHome);
+        lblHome.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // Code to navigate to the Home page (Home.java)
+                dispose(); // Close the current frame
+                Home home = new Home(); // Create an instance of the Home class
+                home.setVisible(true); // Display the Home frame
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                lblHome.setFont(new Font("Maiandra GD", Font.BOLD, 15));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                lblHome.setFont(new Font("Maiandra GD", Font.PLAIN, 15));
             }
         });
-        btnClose.setBounds(174, 377, 89, 40);
-        contentPane.add(btnClose);
 
-        setVisible(true);
+        lblAboutMe = new JLabel("ABOUT ME");
+        lblAboutMe.setForeground(new Color(255, 255, 255));
+        lblAboutMe.setFont(new Font("Maiandra GD", Font.PLAIN, 15));
+        lblAboutMe.setBounds(1045, 45, 107, 50);
+        contentPane.add(lblAboutMe);
+        lblAboutMe.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // Code to navigate to the About Me page (AboutMe.class)
+                System.out.println("About Me clicked");
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                lblAboutMe.setFont(new Font("Maiandra GD", Font.BOLD, 15));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                lblAboutMe.setFont(new Font("Maiandra GD", Font.PLAIN, 15));
+            }
+        });
+
+        lblAchievements = new JLabel("ACHIEVEMENTS");
+        lblAchievements.setFont(new Font("Maiandra GD", Font.PLAIN, 15));
+        lblAchievements.setForeground(new Color(255, 255, 255));
+        lblAchievements.setBounds(1180, 45, 120, 50);
+        contentPane.add(lblAchievements);
+        lblAchievements.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // Code to navigate to the Achievements page (Achievements.class)
+                System.out.println("Achievements clicked");
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                lblAchievements.setFont(new Font("Maiandra GD", Font.BOLD, 15));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                lblAchievements.setFont(new Font("Maiandra GD", Font.PLAIN, 15));
+            }
+        });
+
+        lblContact = new JLabel("CONTACT");
+        lblContact.setForeground(new Color(255, 255, 255));
+        lblContact.setFont(new Font("Maiandra GD", Font.PLAIN, 15));
+        lblContact.setBounds(1350, 45, 107, 50);
+        contentPane.add(lblContact);
+        lblContact.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // Code to navigate to the Contact page (Contact.class)
+                System.out.println("Contact clicked");
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                lblContact.setFont(new Font("Maiandra GD", Font.BOLD, 15));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                lblContact.setFont(new Font("Maiandra GD", Font.PLAIN, 15));
+            }
+        });
     }
 }
